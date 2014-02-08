4ae51b3d83b4be154b65b98fb594a3f7f9acf5e5
Marius Negruțiu <marius.negrutiu@gmail.com>
2014-02-08 09:31:27 +0200

    + Improved queue structure

--- queue.h
+++ queue.h
@@ -16,30 +16,46 @@ typedef enum {
 	ITEM_STATUS_DONE				/// The item has been downloaded (successful or not)
 } ITEM_STATUS;
 
-typedef struct _QUEUE_ITEM {
+typedef enum {
+	ITEM_LOCAL_NONE,				/// The remote content will not be downloaded. Useful for simply connecting (HTTP GET) and then quickly exit
+	ITEM_LOCAL_FILE,				/// The remote content will be downloaded to a local file
+	ITEM_LOCAL_MEMORY				/// The remote content will be downloaded to a memory
+} ITEM_LOCAL_TYPE;
 
-	struct _QUEUE_ITEM *pNext;		/// Singly linked list
+
+typedef struct _QUEUE_ITEM {
 
 	ULONG iId;						/// Unique download ID
 	ITEM_STATUS iStatus;
 
-	TCHAR szURL[1024];
-	TCHAR szFile[1024];
-	ULONG iRetryCount;				/// 0 if not used
-	ULONG iRetryDelay;				/// 0 if not used
+	// Source
+	LPTSTR pszURL;
 
-	BOOL bErrorCodeHTTP;			/// TRUE == HTTP status code, FALSE == Win32 error code
+	// Destination
+	ITEM_LOCAL_TYPE iLocalType;
+	union {
+		LPTSTR pszFile;				/// Valid for ITEM_LOCAL_FILE
+		LPBYTE pMemory;				/// Valid for ITEM_LOCAL_MEMORY. The buffer size will be iFileSize
+	} LocalData;
+
+	// Download options
+	ULONG iRetryCount;				/// (optional)
+	ULONG iRetryDelay;				/// (optional)
+
+	// Runtime statistics
+	FILETIME tmEnqueue;				/// Enqueue time
+	FILETIME tmDownloadStart;		/// Download startup time
+	FILETIME tmDownloadEnd;			/// Download startup time
+	ULONG64 iFileSize;				/// File size or -1 if not available
+	ULONG64 iRecvSize;				/// Received bytes
+
+	// Error code (Win32 or HTTP)
+	BOOL bErrorCodeIsHTTP;			/// TRUE: error code is HTTP status, FALSE: error code is Win32 error
 	ULONG iErrorCode;
 
-	struct {
-		FILETIME tmEnqueue;			/// Enqueue time
-		FILETIME tmDownloadStart;	/// Download startup time
-		FILETIME tmDownloadEnd;		/// Download startup time
-		ULONG64 iFileSize;			/// File size or -1 if not available
-		ULONG64 iRecvSize;			/// Received bytes
-	} Stats;
+	struct _QUEUE_ITEM *pNext;		/// Singly linked list
 
-} QUEUE_ITEM;
+} QUEUE_ITEM, *PQUEUE_ITEM;
 
 
 // Queue locking
@@ -47,30 +63,27 @@ VOID QueueLock();
 VOID QueueUnlock();
 
 
-// Clears the queue, destroys everything.
-// The queue must be locked.
+// Clear the queue, destroy everything
+// The queue must be locked
 BOOL QueueReset();
 
-// Finds a queue item by its ID.
-// The queue must be locked.
-QUEUE_ITEM* QueueFind(
-	_In_ ULONG iItemID
-	);
+// Find an item in the queue
+// The queue must be locked
+PQUEUE_ITEM QueueFind( _In_ ULONG iItemID );	/// ...by ID
+PQUEUE_ITEM QueueFindFirstWaiting();			/// ...by status
 
-// Adds a new queue item.
-// The queue must be locked.
+// Add a new queue item
+// The queue must be locked
 BOOL QueueAdd(
 	_In_ LPCTSTR pszURL,
 	_In_opt_ LPCTSTR pszFile,
-	_Outptr_opt_ QUEUE_ITEM *pItem
+	_Outptr_opt_ PQUEUE_ITEM pItem
 	);
 
-// Removes an item from the queue.
-// The queue must be locked.
-BOOL QueueRemove(
-	_In_ QUEUE_ITEM *pItem
-	);
+// Remove an item from the queue
+// The queue must be locked
+BOOL QueueRemove( _In_ PQUEUE_ITEM pItem );
 
-// Retrieve the number of queue items.
-// The queue must be locked.
-ULONG QueueLength();
+// Retrieve the queue size
+// The queue must be locked
+ULONG QueueSize();
