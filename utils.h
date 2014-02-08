2ffdc560caa5686c3ed2bead12919f806cb878d1
Marius Negruțiu <marius.negrutiu@gmail.com>
2014-02-08 13:11:45 +0200

    + Safer MyFree

--- utils.h
+++ utils.h
@@ -9,7 +9,7 @@ VOID TRACE( __in LPCTSTR pszFormat, ... );
 
 //+ Memory
 #define MyAlloc(_size)		GlobalLock(GlobalAlloc(GMEM_FIXED, _size))
-#define MyFree(_ptr)		{ GlobalFree((HGLOBAL)(_ptr)); (_ptr) = NULL; }
+#define MyFree(_ptr)		if ( _ptr ) { GlobalFree((HGLOBAL)(_ptr)); (_ptr) = NULL; }
 
 #define MyStrDup(_dst, _src) { \
 	(_dst) = (LPTSTR)MyAlloc(( lstrlen( _src ) + 1 ) * sizeof(TCHAR)); \
