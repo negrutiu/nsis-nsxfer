beab7c775bc51c90cebba09f5b4461e44c83c789
Marius Negruțiu <marius.negrutiu@gmail.com>
2014-02-10 22:28:07 +0200

    + Added MyAllocStr and MyZeroMemory

--- utils.h
+++ utils.h
@@ -27,10 +27,16 @@
 
 //+ Memory
 #define MyAlloc(_size)		GlobalLock(GlobalAlloc(GMEM_FIXED, _size))
+#define MyAllocStr(_len)	(LPTSTR)MyAlloc( ((_len) + 1) * sizeof(TCHAR))
 #define MyFree(_ptr)		if ( _ptr ) { GlobalFree((HGLOBAL)(_ptr)); (_ptr) = NULL; }
 
 #define MyStrDup(_dst, _src) { \
 	(_dst) = (LPTSTR)MyAlloc(( lstrlen( _src ) + 1 ) * sizeof(TCHAR)); \
 	if (_dst) \
 		lstrcpy( _dst, _src ); \
+}
+
+#define MyZeroMemory(_ptr, _cnt) { \
+	LPBYTE p; \
+	for ( p = (LPBYTE)(_ptr) + (_cnt) - 1; p >= (LPBYTE)(_ptr); p-- ) *p = 0; \
 }
