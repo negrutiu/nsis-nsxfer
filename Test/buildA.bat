b2781695e75e59f3781af3bb2f251aeed22b870a
Marius Negruțiu <marius.negrutiu@gmail.com>
2014-09-13 09:27:12 +0300

    + Improved build.bat

--- Test/buildA.bat
+++ Test/buildA.bat
@@ -2,3 +2,7 @@
 set NSIS_PATH=%~dp0\..\..\NSISbin
 "%NSIS_PATH%\makensis.exe" /DANSI /V4 "%~dp0\Test.nsi"
 if %ERRORLEVEL% neq 0 pause
+
+echo ----------------------------------------------------------
+set /P answer=Execute NSdownA.exe (y/N)? 
+if /I "%answer%" equ "y" start "NSdown" "%~dp0\NSdownA.exe"
--- Test/buildW.bat
+++ Test/buildW.bat
@@ -1,4 +1,8 @@
 @echo off
 set NSIS_PATH=%~dp0\..\..\NSISbin
 "%NSIS_PATH%\makensis.exe" /V4 "%~dp0\Test.nsi"
-if %ERRORLEVEL% neq 0 pause
+if %ERRORLEVEL% neq 0 pause && goto :EOF
+
+echo ----------------------------------------------------------
+set /P answer=Execute NSdownW.exe (y/N)? 
+if /I "%answer%" equ "y" start "NSdown" "%~dp0\NSdownW.exe"
