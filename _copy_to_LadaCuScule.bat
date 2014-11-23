a092dc684f391e4e4e3d2fc2bf5c165d33aec0d3
Marius Negruțiu <marius.negrutiu@gmail.com>
2014-11-23 20:04:11 +0200

    + Script improvements

--- _copy_to_LadaCuScule.bat
+++ _copy_to_LadaCuScule.bat
@@ -1,7 +1,9 @@
-copy "%~dp0\ReleaseA\NSxfer.dll" "%~dp0\..\LadaCuScule\NSIS.extra\NSxfer"
+mkdir "%~dp0\..\LadaCuScule\NSIS.extra\NSxfer"
+copy "%~dp0\ReleaseA\NSxfer.dll" ""
 copy "%~dp0\ReleaseA\NSxfer.pdb" "%~dp0\..\LadaCuScule\NSIS.extra\NSxfer"
 copy "%~dp0\NSxfer.Readme.txt"   "%~dp0\..\LadaCuScule\NSIS.extra\NSxfer"
 
+mkdir "%~dp0\..\LadaCuScule\NSISw.extra\NSxfer"
 copy "%~dp0\ReleaseW\NSxfer.dll" "%~dp0\..\LadaCuScule\NSISw.extra\NSxfer"
 copy "%~dp0\ReleaseW\NSxfer.pdb" "%~dp0\..\LadaCuScule\NSISw.extra\NSxfer"
 copy "%~dp0\NSxfer.Readme.txt"   "%~dp0\..\LadaCuScule\NSISw.extra\NSxfer"
