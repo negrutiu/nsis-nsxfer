e2d0861b7315edb2af65f8172ac7655e6e394975
Marius Negruțiu <marius.negrutiu@gmail.com>
2016-01-09 21:13:07 +0200

    + Improved script

--- _copy_to_LadaCuScule.bat
+++ _copy_to_LadaCuScule.bat
@@ -1,13 +1,13 @@
-set SRC=%~dp0\ReleaseA-nocrt
-set DST=%~dp0\..\LadaCuScule\NSIS.extra\NSxfer
-mkdir "%DST%"
+@set SRC=%~dp0\ReleaseA-nocrt
+@set DST=%~dp0\..\LadaCuScule\NSIS.extra\NSxfer
+@if not exist "%DST%" mkdir "%DST%"
 copy "%SRC%\NSxfer.dll" "%DST%"
 copy "%SRC%\NSxfer.pdb" "%DST%"
 copy "%~dp0\NSxfer.Readme.txt" "%DST%"
 
-set SRC=%~dp0\ReleaseW-nocrt
-set DST=%~dp0\..\LadaCuScule\NSISw.extra\NSxfer
-mkdir "%DST%"
+@set SRC=%~dp0\ReleaseW-nocrt
+@set DST=%~dp0\..\LadaCuScule\NSISw.extra\NSxfer
+@if not exist "%DST%" mkdir "%DST%"
 copy "%SRC%\NSxfer.dll" "%DST%"
 copy "%SRC%\NSxfer.pdb" "%DST%"
 copy "%~dp0\NSxfer.Readme.txt" "%DST%"
