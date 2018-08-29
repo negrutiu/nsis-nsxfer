c3583f2208905cb70070991310bf756b488dbd7f
Marius Negruțiu <marius.negrutiu@gmail.com>
2018-08-29 21:19:47 +0300

    Relocated to NSIS subdir

--- _copy_to_LadaCuScule.bat
+++ _copy_to_LadaCuScule.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM :: Note: mingw binaries are the most backward compatible (NT4+)
-set DST=%~dp0\..\LadaCuScule
+set DST=%~dp0\..\..\LadaCuScule
 set SRCA=%~dp0\ReleaseA-mingw
 set SRCW=%~dp0\ReleaseW-mingw
 set SRC64=%~dp0\ReleaseW-mingw-amd64
