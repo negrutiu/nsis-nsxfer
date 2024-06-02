f06acddd8c20760d0c7f4cdc31858e82ba5bd0bb
Marius Negruțiu <marius.negrutiu@protonmail.com>
2024-06-02 10:26:01 +0300

    Improved the search for makensis.exe

--- TestDebug/buildW.bat
+++ TestDebug/buildW.bat
@@ -1,15 +1,14 @@
 @echo off
-SetLocal EnableDelayedExpansion
+setlocal enabledelayedexpansion
 
-if not exist "%NSIS%\makensis.exe" pushd "%~dp0..\.." && set NSIS=!CD!&& popd
-if not exist "%NSIS%\makensis.exe" set NSIS=%NSIS_INSTDIR%
+if not exist "%NSIS%\makensis.exe" pushd "%~dp0..\.." && set NSIS=!cd!&& popd
 if not exist "%NSIS%\makensis.exe" set NSIS=%PROGRAMFILES%\NSIS
 if not exist "%NSIS%\makensis.exe" set NSIS=%PROGRAMFILES(X86)%\NSIS
-if not exist "%NSIS%\makensis.exe" echo ERROR: NSIS not found ^(Tip: NSIS_INSTDIR can be defined to point to NSIS binaries^) && pause && exit /B 2
+if not exist "%NSIS%\makensis.exe" for /f "delims=*" %%f in ('where makensis.exe 2^> nul') do pushd "%%~dpf" && set NSIS=!cd!&& popd
+if not exist "%NSIS%\makensis.exe" echo ERROR: NSIS not found&& pause && exit /b 2
 
 echo ********************************************************************************
 echo %NSIS%\makensis.exe
 echo ********************************************************************************
 
-"%NSIS%\makensis.exe" /V4 "%~dp0\NSxfer-Debug.nsi"
-if %errorlevel% neq 0 pause && exit /B %errorlevel%
+"%NSIS%\makensis.exe" /V4 "%~dp0\NSxfer-Debug.nsi" || pause && exit /b !errorlevel!
